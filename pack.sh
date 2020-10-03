4f068ded5b3a6c83605b4a27acdfd3526abf24b2
Stefan Hackenberg <mail@stefan-hackenberg.de>
2020-10-03 18:22:24 +0200

    Add pack.sh

---
+++ pack.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ -d download ]; then
+    rm -rf download
+fi
+mkdir download
+pushd download
+cp ../emacs-jit*.deb .
+wget https://github.com/d12frosted/elpa-mirror/archive/master.zip -O elpa-mirror-master.zip
+git clone https://github.com/syl20bnr/spacemacs.git -b develop
+popd
+
+zipname="emacs-jit_$(date '+%Y%m%d').zip"
+zip -qr $zipname download
+cp $zipname /var/www/download/
